f4c9fd29dfb88b23d49d9820d87bd7b53f76f05c
Silvestrs <46748607+SilvisPilvis@users.noreply.github.com>
2023-05-16 15:24:15 +0100

    Now adds user to docker group.

--- install-docker.sh
+++ install-docker.sh
@@ -2,5 +2,7 @@
 
 curl -sSL https://get.docker.com | sh
 sleep 2
+echo "Ading $USER to docker group."
+sudo usermod -aG docker $USER
 echo "Installing portainer."
 sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:linux-arm
